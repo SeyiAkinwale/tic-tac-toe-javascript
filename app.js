e4c59c646f8ba260710086e8e6b8c983c00b147b
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-08-21 22:51:50 +0000

    game is functional

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const startCells =
   "", "", "", "", "", "", "", "", "",
 ]
 
+let go = "circle"
+infoDisplay.textContent = "Circle goes first"
 function createBoard() 
 {
   startCells.forEach((_cell, index) => 
@@ -22,6 +24,50 @@ createBoard()
 function addGo(e)
 {
   const goDisplay = document.createElement('div')
-  goDisplay.classList.add('cross')
+  goDisplay.classList.add(go)
   e.target.appendChild(goDisplay)
+  go = go === "circle" ? "cross" : "circle"
+  infoDisplay.textContent = "it is now " + go + "'s go."
+  e.target.removeEventListener("click", addGo)
+  checkScore()
+}
+
+function checkScore()
+{
+  const allSquares = document.querySelectorAll(".square")
+  const winningCombos =
+    [
+      [0,1,2], [3,4,5], [6,7,8],
+      [0,3,6], [1,4,7], [2,5,8],
+      [0,4,8], [2,4,6]
+    ]
+
+  winningCombos.forEach(array=>
+  {
+    const circleWins = array.every(cell =>     
+      allSquares[cell].firstChild?.classList.contains('circle'))
+    
+      if(circleWins)
+      {
+      infoDisplay.textContent = "Circle Wins"
+      allSquares.forEach(square => 
+        square.replaceWith(square.cloneNode(true)))
+      return
+      }    
+  })
+
+  winningCombos.forEach(array=>
+  {
+      const crossWins = array.every(cell =>     
+        allSquares[cell].firstChild?.classList.contains('cross'))
+    
+    if(crossWins)
+    {
+      infoDisplay.textContent = "Cross Wins"
+      allSquares.forEach(square => 
+        square.replaceWith(square.cloneNode(true)))
+      return
+    }
+      
+  })
 }
